09b13d85b640b9fd6aa0dd6b5218ad4237a55c2b
Jessica-21-Simone <jessica.nana2@polymtl.ca>
2024-09-04 16:37:33 -0400

    Réponse a la question 1 completee

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@ import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
 
+Aire=(math.pi*rayon**2)
+print(Aire)
+
+
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Jessica Nana")
+
